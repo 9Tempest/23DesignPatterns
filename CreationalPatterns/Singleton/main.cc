0f97ff0c9d0418163e5cd0d34de4d6bc3883f195
9tempest <z277zhu@uwaterloo.ca>
2020-08-29 19:56:55 +0800

    Removed duplicated

--- CreationalPatterns/Singleton/main.cc
+++
@@ -1,8 +0,0 @@
-#include "singleton.h"
-#include <iostream>
-int main(int argc, char const *argv[])
-{
-    Singleton* instance = Singleton::getInstance();
-    std::cout << instance->getData() << std::endl;
-    return 0;
-}
